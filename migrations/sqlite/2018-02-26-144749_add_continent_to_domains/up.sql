444f6fb7c0f51d1b7aa7099b9203586d188bb84a
napat1412 <napat.chu@gmail.com>
2021-07-05 15:42:13 +0700

    Change continent datebase schema from var(2) to var(6)

--- migrations/sqlite/2018-02-26-144749_add_continent_to_domains/up.sql
+++ migrations/sqlite/2018-02-26-144749_add_continent_to_domains/up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE domains ADD COLUMN continent VARCHAR(2) NOT NULL DEFAULT '';
+ALTER TABLE domains ADD COLUMN continent VARCHAR(6) NOT NULL DEFAULT '';
 UPDATE domains SET continent = 'NA' WHERE continent = '';
